8da24ef9d41de1b68db41a779b8b744bb6aabea8
Boris Garcevic <borisgarcevic@gmail.com>
2026-09-18 10:38:16 +0200

    Add Linux ARM64 release artifacts, install.sh and self-update support (#94)

--- install.sh
+++ install.sh
@@ -102,6 +102,9 @@ case "$(uname -s):$(uname -m)" in
   Linux:x86_64 | Linux:amd64)
     target=x86_64-unknown-linux-gnu
     ;;
+  Linux:aarch64 | Linux:arm64)
+    target=aarch64-unknown-linux-gnu
+    ;;
   Darwin:arm64 | Darwin:aarch64)
     target=aarch64-apple-darwin
     ;;
